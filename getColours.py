dc2bb52c3469128aa11bb56ed07ce7ce17eadd3b
noelmurasko <murasko@ualberta.ca>
2022-07-14 12:25:01 -0600

    Added rgb and cmyk to GargoyleColours.txt.

--- getColours.py
+++ getColours.py
@@ -2,7 +2,6 @@ import os
 
 # Used to collect the colours in GargoyleColours.txt. Could in principle 
 # be used to collect colours from any pallete organized in this way.
-# In the future, you could also use this compute other color formats such as CYMK.
 
 # The pallete function returns a list of Colours. Each colour has 3 strings:
 # a description, name, and hex. 
@@ -11,9 +10,28 @@ import os
 class Colour:
 
 	def __init__(self, description, name, hexCode):
-		self.desc = description
-		self.name = name
-		self.hex = hexCode
+		self.desc=description
+		self.name=name
+		self.hex=hexCode
+
+		# Integers between 0 and 255.
+		R=int(hexCode[1:3],16)
+		G=int(hexCode[3:5],16)
+		B=int(hexCode[5:7],16)
+
+		self.rgb="("+str(R)+", "+str(G)+", "+str(B)+")"
+
+		# Floats between 0 and 1.
+		Rf=R/255
+		Bf=B/255
+		Gf=G/255
+
+		K=1-max(Rf,Bf,Gf)
+		C=int((1-Rf-K)/(1-K)*100)
+		M=int((1-Gf-K)/(1-K)*100)
+		Y=int((1-Bf-K)/(1-K)*100)
+		K=int(K*100)
+		self.cymk="("+str(C)+"%, "+str(M)+"%, "+str(Y)+"%, "+str(K)+"%)"
 
 def pallete():
 	pallete = []
@@ -21,8 +39,8 @@ def pallete():
 	text=file.read()
 	start=text.find('----',0)+5
 	while start < len(text):
-		b1 = text.find('|',start)
-		b2 = text.find('|',b1+1)
+		b1=text.find('|',start)
+		b2=text.find('|',b1+1)
 		description=text[start:b1-1]
 		if description[0]=="\n":
 			description = description[1:]
